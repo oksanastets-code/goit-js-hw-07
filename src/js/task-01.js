079335683fbae6db463418de19b7182cc848fc4d
Oksana Stets <oksanastets73@gmail.com>
2021-09-12 22:52:35 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelector('#categories');
+const categoriesItems = categories.children;
+const quantityCategoriesMessage = `В списке ${categoriesItems.length} категории.`
+console.log(quantityCategoriesMessage);
+
+console.log(categoriesItems);
+
+
+
