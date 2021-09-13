99ab80a707c338b2abbf424ff3fb657545d7dafb
Oksana Stets <oksanastets73@gmail.com>
2021-09-13 11:59:58 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,12 @@
 const categories = document.querySelector('#categories');
-const categoriesItems = categories.children;
+const categoriesItems = [...categories.children];
 const quantityCategoriesMessage = `В списке ${categoriesItems.length} категории.`
 console.log(quantityCategoriesMessage);
 
-console.log(categoriesItems);
+const message = categoriesItems.map(item => {
+const categoriesItemTitle = item.querySelector('h2');
+console.log('Категория:', categoriesItemTitle.textContent);
 
-
-
+const categoriesItemList = item.querySelector('ul');
+console.log('Количество элементов:', categoriesItemList.children.length);
+})
