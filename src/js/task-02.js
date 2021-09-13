2998ef03eb5356368f156eac9277caa1fd1ba976
Oksana Stets <oksanastets73@gmail.com>
2021-09-13 12:50:22 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const elements = ingredients.map(el => {
+  const item = document.createElement('li');
+  item.textContent = el;
+  return item;
+});
+
+const list = document.querySelector('#ingredients');
+list.append(...elements);
