e6e83ee563c111ba7f5f979f1e1c0256a8fb1163
Oksana Stets <oksanastets73@gmail.com>
2021-09-15 17:25:44 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const refs = {
+    inputField: document.querySelector('#name-input'),
+    outputText: document.querySelector('#name-output'),
+}
+
+refs.inputField.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    if (event.currentTarget.value != '') {
+        refs.outputText.textContent = event.currentTarget.value;
+    } else refs.outputText.textContent = 'незнакомец';
+};
