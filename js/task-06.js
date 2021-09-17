2423b7d6d837f94802d2e384a8a4d6b46c8b6059
Oksana Stets <oksanastets73@gmail.com>
2021-09-17 17:54:44 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,10 @@ inputText.addEventListener('blur', onInputBlur);
 function onInputBlur(event) {
     
     if (event.currentTarget.value.length === Number(inputText.dataset.length)) {
-        inputText.classList.add('valid')
+        inputText.classList.add('valid');
+        inputText.classList.remove('invalid');
     }
-    else inputText.classList.add('invalid')
+    else {
+        inputText.classList.add('invalid');
+    }
 }
