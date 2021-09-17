234ba73706eb79ea3d7195b5e4d962b2e1fa4bd0
Oksana Stets <oksanastets73@gmail.com>
2021-09-17 13:03:16 +0300

    Update task-08.js

---
+++ js/task-08.js
@@ -0,0 +1,45 @@
+const controlPanel = document.querySelector('#controls');
+const inputValueEl = document.querySelector('#controls input');
+const renderBtn = document.querySelector('button[data-action="render"]');
+const destroyBtn = document.querySelector('button[data-action="destroy"]');
+const boxesContainer = document.querySelector('#boxes');
+
+inputValueEl.addEventListener('input', onInputValue);
+function onInputValue(event) {
+};
+
+renderBtn.addEventListener('click', onRenderBtnClick);
+function onRenderBtnClick() {
+    const amount = inputValueEl.value;
+    createBoxes(amount);
+};
+
+destroyBtn.addEventListener('click', onDestroyBtnClick);
+function onDestroyBtnClick() {
+    inputValueEl.value = '';
+    boxesContainer.innerHTML = '';
+    console.log(boxesContainer);
+}
+
+function random() {
+    return Math.round(Math.random() * 256);
+}
+
+const width = 30;
+const height = 30;
+
+function createBoxes(amount) {
+    const boxes = [];
+
+    for (let i = 0; i < amount; i += 1) {
+        const box = document.createElement('div');
+        box.style.height = height + 10 *i +'px';
+        box.style.width = width + 10 * i + 'px';
+        box.style.marginBottom = '10px';
+        box.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
+        
+        boxes.push(box);
+    }
+    boxesContainer.append(...boxes);
+    console.log(boxesContainer);
+}
